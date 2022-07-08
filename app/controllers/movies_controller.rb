862e40218a12b5f118569c0cf32b1be93e3e3f2a
thiago aragao <thiagohoaragao@gmail.com>
2022-07-07 22:09:54 -0300

    added conditional if no query is passed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,16 @@
 class MoviesController < ApplicationController
   def find
-    @movies = params[:query].present? ? Movie.search(params[:query]) : Movie.all.order(year: :desc)
+    filter = params.slice(:year, :title, :genre, :country)
+    if !filter.empty?
+      @movies = Movie.search(filter).order(year: :desc)
+    else
+      @movies = Movie.all.order(year: :desc)
+    end
     render json: @movies
   end
 
   def find_by_id
+    @movies = Movie.find(params[:id].to_s)
+    render json: @movies
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,19 @@
 class Movie < ApplicationRecord
   validates :title, uniqueness: true
-  def self.search(search)
-    where('title like :search OR genre LIKE :search OR year like :search OR country like :search',
-          search: "%#{search}%")
+
+  def self.search(filter)
+    query_string = []
+    like_filter = {}
+    filter.each do |key, value|
+      if key != 'year'
+        query_string << "#{key} LIKE :#{key}"
+        like_filter[key.to_sym] = "%#{value}%"
+      else
+        query_string << "#{key} = :#{key}"
+        like_filter[key.to_sym] = "#{value}"
+      end
+    end
+    query_string = query_string.join(' and ')
+    where(query_string, like_filter)
   end
 end
